96f631a4fb3fe6212677d1845c4f2652f0207bbd
github keei0920 <yellowsubmarine.0920@gmail.com>
2024-01-03 17:03:55 +0900

    tweetの削除、編集、詳細機能の実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,6 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:edit, :show]
+
   def index 
     @tweets = Tweet.all
   end
@@ -12,7 +14,29 @@ class TweetsController < ApplicationController
     redirect_to root_path
   end
 
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+    redirect_to root_path
+  end
+
+  def edit
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    tweet.update(tweet_params)
+    redirect_to root_path
+  end
+
+  def show
+  end
+
   private
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+  end
 
   def tweet_params
     params.require(:tweet).permit(:name, :image, :text)
